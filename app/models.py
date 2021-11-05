6672eb28c05a76c886493d76a092825c3503ffc3
Etienne Devictor <etienne.devictor@sjsu.edu>
2021-11-05 14:32:57 -0700

    database- made the User table to store users and made a repr method, and methods that check if email/username already exists

--- app/models.py
+++ app/models.py
@@ -1,1 +1,22 @@
-models
+from app import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(128), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+
+	def __repr__(self):
+		return f'<User {username}>'
+
+	def username_exists(self, username):
+		for user in User:
+			if (username == user.username):
+				return True
+		return False
+
+	def email_exists(self, email):
+		for user in User:
+			if (email == user.email):
+				return True
+		return False
