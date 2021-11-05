8ec015a248a04c42142516d756f420c056187807
Etienne Devictor <etienne.devictor@sjsu.edu>
2021-11-05 14:51:50 -0700

    routes- added all the user related imports to routes and added a route to the signup page

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,9 @@
-routes
+from app import db
+from app.models import User
+from myapp.forms import LoginForm
+from flask import render_template, escape, flash, redirect
+from flask_login import current_user, login_user, login_required
+
+@myobj.route('/signup")
+def signup():
+	return render_template("signup.html")
