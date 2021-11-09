a367ee7c9559a0deb6293f85a2066ec8bf945c59
EtienneDevictor <devictoretienne@gmail.com>
2021-11-09 09:59:32 -0800

    added redirect to home after a user has registered

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,12 @@ from app.forms import LoginForm
 from flask import render_template, escape, flash, redirect
 from flask_login import current_user, login_user, login_required
 
-@app_obj.route('/signup')
+@app_obj.route('/signup', methods=['Get', 'Post'])
 def signup():
     header = 'Register Account'
     form = LoginForm()
     if form.validate_on_submit():
-        flash(str ="attempting to create an account")
+        flash("attempting to create an account")
         if User.email_exists(form.email.data):
             flash('The provided email already belong to an account')
         elif User.username_exists(form.username.data):
@@ -21,6 +21,7 @@ def signup():
                         password_hash=password_hash)
             db.session.add(user)
             db.session.commit(user)
+            redirect('/')
 	return render_template("signup.html", 
                         header=header, 
                        	title='sign up page', 
