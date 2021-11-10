6f40d227efb2739c1d02bc8b0157e2293843ed25
Benson Jiang <benson.jiang@sjsu.edu>
2021-11-10 00:08:12 -0800

    Fixed no validate bug, username validation bug

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class SignInForm(FlaskForm):
 	username = StringField('User', validators=[DataRequired()])
 	email = StringField('email', validators=[DataRequired()])
 	password = PasswordField('password', validators=[DataRequired()])
-	remember_me = BooleanField('Remember Me')
 
-	submit = SubmitField('Sign in')
 	register = SubmitField('Sign up')
         
+class LoginForm(FlaskForm):
+	username = StringField('User', validators=[DataRequired()])
+	password = PasswordField('password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember me')
+
+	submit = SubmitField('Login')
 	delete = SubmitField('Delete Account')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import db, app_obj
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, SignInForm
 from flask import render_template, escape, flash, redirect
 from flask_login import current_user, login_user, login_required
+
+@app_obj.route('/')
+def home():
+    return redirect('/signup')
 
 @app_obj.route('/deleteaccount', methods=['GET', 'POST'])
 def delete():
@@ -11,7 +15,7 @@ def delete():
         user = User.query.filter_by(username=form.username.data)
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')	
-            redirect ('/deleteaccount') 
+            return redirect ('/deleteaccount') 
         db.session.delete(user)
         db.session.commit()
         flash(f'{form.username} has been deleted')
@@ -20,7 +24,7 @@ def delete():
 @app_obj.route('/signup', methods=['GET', 'POST'])
 def signup():
     header = 'Register Account'
-    form = LoginForm()
+    form = SignInForm()
     if form.validate_on_submit():
         flash("attempting to create an account")
         user = User.query.filter_by(username=form.username.data)
@@ -34,7 +38,7 @@ def signup():
             user = User(username=form.username.data, email=form.email.data, password_hash=password_hash)
             db.session.add(user)
             db.session.commit(user)
-            redirect('/')
+            return redirect('/')
     return render_template("signup.html", header=header, title='Sign Up Page', form=form)
 
 @app_obj.route('/login', methods = ['GET', 'POST'])
@@ -42,11 +46,10 @@ def login():
     title = 'Login'
     form = LoginForm()
     if form.validate_on_submit():
-        redirect('/deleteaccount')
-        user = User.query.filter_by(username = form.username.data)
+        user = User.query.filter_by(username = form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            redirect('/')
+            return redirect('/login')
         login_user(user, remember  = form.remember_me.data)
     return render_template("login.html", title = title, form = form)
 
