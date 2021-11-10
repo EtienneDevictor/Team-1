4990e2f7cc074badde85d838819a8bad296e492b
Benson Jiang <benson.jiang@sjsu.edu>
2021-11-09 22:43:54 -0800

    created login.html and added to routes.py

--- app/routes.py
+++ app/routes.py
@@ -34,3 +34,15 @@ def signup():
             db.session.commit(user)
             redirect('/')
     return render_template("signup.html", header=header, title='Sign Up Page', form=form)
+
+@app_obj.route('/login', methods = ['GET', 'POST'])
+def login():
+    title = 'Login'
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data)
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            redirect('/login')
+        login_user(user, remember  = form.remember_me.data)
+    return render_template("login.html", title = title, form = form)
