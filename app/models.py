ca56549db4b20070ccdf791c5e0009c91f0d2276
EtienneDevictor <devictoretienne@gmail.com>
2021-11-16 16:02:54 -0800

    fixed database its working now

--- app/models.py
+++ app/models.py
@@ -4,12 +4,11 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    flashCard = db.relationship('CardList', backref='author', lazy='dynamic' )
+    classes = db.relationship('Class', backref='author', lazy='dynamic')
     is_active = False
 
     def __repr__(self):
@@ -21,12 +20,11 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
         
-class Class(UserMixin, db.Model):
-    __tablename__ = 'classes'
+class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(128), index=True)
-    cardlist = db.relationship('CardList', backref='author', lazy='dynamic')
+    cardlist = db.relationship('Cardlist', backref='author', lazy='dynamic')
     notes = db.relationship('Notes', backref='author', lazy='dynamic')
     
     def __repr__(self):
@@ -34,26 +32,25 @@ class Class(UserMixin, db.Model):
     
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
     title = db.Column(db.String(128), index=True)
     mdFilePath = db.Column(db.String(128), index=True)
     
     def __repr__(self):
         return f'{self.title}'
-  
-class CardList(UserMixin, db.Model) :
-    __tablename__ = 'cardlists'
+     
+class Cardlist(db.Model) :
     id = db.Column(db.Integer, primary_key=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
     title = db.Column(db.String(128), index=True)
-    flashCard = db.relationship('Class', backref='author', lazy='dynamic')
+    flashCard = db.relationship('FlashCard', backref='author', lazy='dynamic')
  
     def __repr__(self):
         return f'{self.title}'
-
+    
 class FlashCard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cardList_id = db.Column(db.Integer, db.ForeignKey('cardlists.id'))
+    cardList_id = db.Column(db.Integer, db.ForeignKey('cardlist.id'))
     title = db.Column(db.String(256), index=True)
     content = db.Column(db.String(256), index=True)
     imagePath = db.Column(db.String(128), index=True)
@@ -62,9 +59,6 @@ class FlashCard(db.Model):
         return self.title
     
      
- 
-
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
